bd499e381c8b450fc3d19b8b5aef045d4a686154
Cristhian Alvarez <gerardoalvares354@gmail.com>
2025-11-20 22:20:19 -0600

    Añadiendo colorpalette principal de la app

---
+++ HotelFind/theme/color.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  vibrantOrange: '#FF8000',
+  deepBlue: '#004DCC',
+  darkGray: '#333333',
+  pureWhite: '#FFFFFF',
+  goldenYellow: '#FFBF00',
+};
---
+++ HotelFind/theme/index.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    vibrantOrange: '#FF8000',
+    deepBlue: '#004DCC',
+    darkGray: '#333333',
+    pureWhite: '#FFFFFF',
+    goldenYellow: '#FFBF00',
+  },
+};
